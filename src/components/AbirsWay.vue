13894440a0538d0e0fec6894559c42236e2f6a45
Abeer <abeer.technology@gmail.com>
2021-03-01 14:21:39 +0600

    AbirsWay design complete

---
+++ src/components/AbirsWay.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="row">
+    <!-- Calculate Time blocks -->
+    <div class="cal-blocks col-6 mr-2">
+      <p class="input-text">Insert time-blocks:</p>
+      <textarea
+        class="block-input d-block rounded w-100 p-2"
+        rows="5"
+      ></textarea>
+      <button class="btn btn-success btn-calculate mt-2 w-100">
+        CALCULATE
+      </button>
+
+      <!-- Result -->
+      <div class="card shadow-sm mt-5">
+        <div class="card-header alert alert-success h4 text-center">
+          TOTAL TIME
+        </div>
+        <div class="card-body">
+          <h4 class="card-title text-center">00:00</h4>
+        </div>
+      </div>
+    </div>
+
+    <!-- Calculate average time -->
+    <div class="cal-avg col-6 ml-2"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.input-text {
+  font-size: 20px;
+}
+.btn-calculate {
+  font-family: "Concert One", cursive;
+  font-size: 24px;
+  font-weight: 200;
+}
+.card-header {
+  font-family: "Concert One", cursive;
+}
+.block-input {
+  font-family: "Roboto", sans-serif;
+  font-size: 20px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <!-- Head Grid => for logo and categories -->
+    <div class="head-grid">
+      <div class="row mt-3">
+        <!-- Logo -->
+        <div class="logo col-6">
+          <h2>Time Calculator</h2>
+        </div>
+        <!-- Categories -->
+        <div class="categories text-right col-6">
+          <div class="btn-menu btn btn-success">
+            MENU <i class="fas fa-bars"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End of Head Grid -->
+
+    <!-- Deafult Calculation page -->
+    <abirs-way class="mt-3"></abirs-way>
+  </div>
+</template>
+
+<script>
+import AbirsWay from "./AbirsWay.vue";
+
+export default {
+  components: {
+    AbirsWay,
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  font-family: "Satisfy", cursive;
+}
+.btn-menu {
+  font-weight: bold;
+}
+</style>;
